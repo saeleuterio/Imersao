951a77c4859d475afb29761b2a2f72a56a8e385b
saeleuterio <saeleuterio@hotmail.com>
2024-09-04 20:16:44 -0300

    alura

--- dados.js
+++ dados.js
@@ -1,1 +1,18 @@
-let dados = "Rayssa Leal"
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce e carisma. Aos poucos anos, ela já acumula títulos importantes e inspira jovens skatistas em todo o Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é um dos grandes nomes do judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances impecáveis. Medalhista olímpica, ela é um exemplo de superação e dedicação.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+    }
+];
+
