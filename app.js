f1dffba65f20d0b9afe56d35f8bd3a4048554e14
saeleuterio <saeleuterio@hotmail.com>
2024-09-05 18:02:00 -0300

    alura

--- app.js
+++ app.js
@@ -3,4 +3,12 @@
 let section = document.getElementById("resultados-pesquisa");
 console.log(section);
 
-
+section.innerHTML = `
+    <div class="item-resultado">
+        <h2>
+            <a href="#" target="_blank">San Francisco 49ers</a>
+        </h2>
+        <p class="descricao-meta">Os San Francisco 49ers são uma das franquias mais tradicionais e bem-sucedidas da National Football League (NFL). Com uma rica história e uma torcida apaixonada, os 49ers conquistaram diversos títulos ao longo dos anos, sendo uma das equipes mais populares dos Estados Unidos.</p>
+        <a href="https://pt.wikipedia.org/wiki/San_Francisco_49ers" target="_blank">Mais informações</a> 
+    </div>
+`
