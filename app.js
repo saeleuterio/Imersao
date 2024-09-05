85318c4e757bb9dac79905e63a1ff029e84cf3da
saeleuterio <saeleuterio@hotmail.com>
2024-09-05 18:40:35 -0300

    alura

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 function pesquisar() {
+    // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
-console.log(section);
-
-let resultados = ""
-
-//para cada dado dentro da lista de dados
-for(let dado of dados) {
-    resultados += `
-    <div class="item-resultado">
-        <h2>
+    console.log(section); // Exibe o elemento HTML selecionado no console
+  
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array de objetos)
+    for (let dado of dados) {
+    // Constrói o HTML para cada item do resultado da pesquisa
+        resultados += `
+        <div class="item-resultado">
+            <h2>
             <a href="#" target="_blank">${dado.titulo}</a>
-        </h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href=${dado.link} target="_blank">Mais informações</a> 
-    </div>
-`
-}
-
-section.innerHTML = resultados
-
-}
-//console.log(dados);
-
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML gerado para o conteúdo da seção
+    section.innerHTML = resultados;
+  }
