04a4f629ac97dfb081c780eb256deebce92a253b
saeleuterio <saeleuterio@hotmail.com>
2024-09-05 09:23:39 -0300

    alura

--- dados.js
+++ dados.js
@@ -1,18 +1,18 @@
 let dados = [
     {
-        titulo: "Rayssa Leal",
-        descricao: "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce e carisma. Aos poucos anos, ela já acumula títulos importantes e inspira jovens skatistas em todo o Brasil.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        titulo: "San Francisco 49ers",
+        descricao: "Os San Francisco 49ers são uma das franquias mais tradicionais e bem-sucedidas da National Football League (NFL). Com uma rica história e uma torcida apaixonada, os 49ers conquistaram diversos títulos ao longo dos anos, sendo uma das equipes mais populares dos Estados Unidos.",
+        link: "https://pt.wikipedia.org/wiki/San_Francisco_49ers",
     },
     {
-        titulo: "Beatriz Souza",
-        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é um dos grandes nomes do judô feminino brasileiro.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+        titulo: "Green Bay Packers",
+        descricao: "Os Green Bay Packers são uma das franquias mais históricas e apaixonantes da NFL. Com sede em Green Bay, Wisconsin, os Packers são conhecidos por sua forte conexão com a comunidade e por suas diversas conquistas ao longo dos anos.",
+        link: "https://pt.wikipedia.org/wiki/Green_Bay_Packers" 
     },
     {
-        titulo: "Rebeca Andrade",
-        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances impecáveis. Medalhista olímpica, ela é um exemplo de superação e dedicação.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+        titulo: "Tampa Bay Buccaneers",
+        descricao: "Os Tampa Bay Buccaneers são uma franquia da NFL com uma história relativamente curta, mas repleta de momentos marcantes. Fundados em 1976, os Bucs, como são carinhosamente chamados, conquistaram seu espaço na liga e se tornaram uma força a ser considerada.",
+        link: "https://pt.wikipedia.org/wiki/Tampa_Bay_Buccaneers" 
     }
 ];
 
