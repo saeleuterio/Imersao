b5004ac3ccde22ada8ab9d60159def16101a4954
saeleuterio <saeleuterio@hotmail.com>
2024-09-05 18:19:44 -0300

    alura

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 let section = document.getElementById("resultados-pesquisa");
 console.log(section);
 
-section.innerHTML = `
+//para cada dado dentro da lista de dados
+for(let dado of dados) {
+    section.innerHTML += `
     <div class="item-resultado">
         <h2>
-            <a href="#" target="_blank">San Francisco 49ers</a>
+            <a href="#" target="_blank">${dado.titulo}</a>
         </h2>
-        <p class="descricao-meta">Os San Francisco 49ers são uma das franquias mais tradicionais e bem-sucedidas da National Football League (NFL). Com uma rica história e uma torcida apaixonada, os 49ers conquistaram diversos títulos ao longo dos anos, sendo uma das equipes mais populares dos Estados Unidos.</p>
-        <a href="https://pt.wikipedia.org/wiki/San_Francisco_49ers" target="_blank">Mais informações</a> 
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais informações</a> 
     </div>
 `
+}
+
+
