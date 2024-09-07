8a973b0b71afdc144e28a0f3f28334533b741d06
saeleuterio <saeleuterio@hotmail.com>
2024-09-06 21:03:25 -0300

    alura

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 function pesquisar() {
     // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
-    console.log(section); // Exibe o elemento HTML selecionado no console
-  
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // Se o campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+      section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome do time da NFL</p>"
+      return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
   
     // Itera sobre cada dado na lista de dados (assumindo que 'dados' é um array de objetos)
     for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      // Se titulo includes campoPesquisa
+      if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
     // Constrói o HTML para cada item do resultado da pesquisa
         resultados += `
         <div class="item-resultado">
@@ -18,6 +35,11 @@ function pesquisar() {
             <a href=${dado.link} target="_blank">Mais informações</a>
         </div>
       `;
+      }       
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
     }
   
     // Atribui o HTML gerado para o conteúdo da seção
--- dados.js
+++ dados.js
@@ -3,16 +3,19 @@ let dados = [
         titulo: "San Francisco 49ers",
         descricao: "Os San Francisco 49ers são uma das franquias mais tradicionais e bem-sucedidas da National Football League (NFL). Com uma rica história e uma torcida apaixonada, os 49ers conquistaram diversos títulos ao longo dos anos, sendo uma das equipes mais populares dos Estados Unidos.",
         link: "https://pt.wikipedia.org/wiki/San_Francisco_49ers",
+        tags: "bola eua 49"
     },
     {
         titulo: "Green Bay Packers",
         descricao: "Os Green Bay Packers são uma das franquias mais históricas e apaixonantes da NFL. Com sede em Green Bay, Wisconsin, os Packers são conhecidos por sua forte conexão com a comunidade e por suas diversas conquistas ao longo dos anos.",
-        link: "https://pt.wikipedia.org/wiki/Green_Bay_Packers" 
+        link: "https://pt.wikipedia.org/wiki/Green_Bay_Packers",
+        tags: "verde branco"
     },
     {
         titulo: "Tampa Bay Buccaneers",
         descricao: "Os Tampa Bay Buccaneers são uma franquia da NFL com uma história relativamente curta, mas repleta de momentos marcantes. Fundados em 1976, os Bucs, como são carinhosamente chamados, conquistaram seu espaço na liga e se tornaram uma força a ser considerada.",
-        link: "https://pt.wikipedia.org/wiki/Tampa_Bay_Buccaneers" 
+        link: "https://pt.wikipedia.org/wiki/Tampa_Bay_Buccaneers",
+        tags: "bucs mar florida"
     }
 ];
 
